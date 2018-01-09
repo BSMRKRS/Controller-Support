77436d1c5c6eaaff9776dcd2a7d6c83d3404b0a9
avoss19 <31517943+avoss19@users.noreply.github.com>
2018-01-09 16:43:49 -0600

    less latency

--- clientRead.py
+++ clientRead.py
@@ -9,8 +9,6 @@ while True:
     ftp.cwd('directory of this repository') # directory of repo on host
     gFile = open("ftpTemp.txt", "wb")
     ftp.retrbinary('RETR ftpTemp', gFile.write)
-    gFile.close()
-    ftp.quit()
     gFile = open("ftpTemp.txt", "r")
     buff = gFile.read()
     convertTxtArray = buff.split()
