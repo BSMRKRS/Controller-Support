22a51665d0e28155ca41fae4f1a5936c51781061
Andrew Voss <voss1167@gmail.com>
2018-01-26 20:43:15 -0600

    less latency & one less temp file

--- clientRead.py
+++ clientRead.py
@@ -1,18 +1,26 @@
 # --------------------------------File on client--------------------------------
-# Reads
-
+# Reads the ftp file from Host
 import RoboPiLib as RPL
 import ftplib
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+######################
+##    Host Info     ##
+######################
+ftp = ftplib.FTP('ip', 'username', 'password') # host computer info
+ftp.cwd('directory of this repo') # directory of repo on host
+
+######################
+##      Main        ##
+######################
 while True:
-    ftp = ftplib.FTP('ip', 'username', 'password') # host computer info
-    ftp.cwd('directory of this repository') # directory of repo on host
-    gFile = open("ftpTemp.txt", "wb")
+    gFile = open("ftpTemp", "wb")
     ftp.retrbinary('RETR ftpTemp', gFile.write)
-    gFile = open("ftpTemp.txt", "r")
+    gFile = open("ftpTemp", "r")
     buff = gFile.read()
     convertTxtArray = buff.split()
     motorL = convertTxtArray[0]
     motorR = convertTxtArray[1]
     RPL.servoWrite(0, int(float(motorL))) # for some odd reason would not convert directly to int with int()
     RPL.servoWrite(1, int(float(motorR))) # for some odd reason would not convert directly to int with int()
+    print motorL, motorR
