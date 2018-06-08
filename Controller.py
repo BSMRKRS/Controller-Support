15c4ecc4cbe41c155699ac2260b790a66038fc64
avoss19 <avoss19@bsmschool.org>
2018-06-07 19:55:57 -0500

    Arm Branch init

--- Controller.py
+++ Controller.py
@@ -6,6 +6,8 @@ from time import sleep
 
 #### Global Variables ####
 
+mode = 1 # starting mode; 1 for driving and 0 for arm
+
 socketRate = .1 # Make larger number to slow do info sent to Robot; larger number creates more latency; Too low of number sents too much info
 
 # left and right joystick dead zones (current dead zone for ps4 controller)
@@ -30,6 +32,7 @@ pygame.joystick.init()
 ## 1. UI
 ######################
 def ui():
+    global controllerScheme
     print "#"*60
     print "Welcome to the BSM robot controller support python program!"
     print "#"*60
@@ -39,6 +42,8 @@ def ui():
     print "For support please visit https://github.com/BSMRKRS/Controller-Support.git"
     print "#"*60
     print "To controll use the left and right joystick."
+    print "Hit xbox button to quit."
+    print "#"*60
     print "Hit Enter to begin!"
     raw_input("$: ")
     print "#"*60
@@ -49,6 +54,8 @@ def ui():
 ######################
 def controllerInput():
     global xAxisLeft, yAxisLeft, xAxisRight, yAxisRight, triggerLeft, triggerRight
+    global buttonSquare, buttonX, buttonCircle, buttonTriangle
+    global aButton, start, xbox
 
     dpadleft = 0
     dpadright = 0
@@ -72,6 +79,11 @@ def controllerInput():
     xAxisRight = joystick.get_axis(2)
     yAxisRight = joystick.get_axis(3)
 
+    triggerLeft = joystick.get_axis(4)
+    triggerRight = joystick.get_axis(5)
+
+    start = joystick.get_button(4)
+    xbox = joystick.get_button(10)
 
 ######################
 ## 3. Inturpret Joystick
@@ -109,41 +121,88 @@ def KitBotSpeed(speed):
 
 
 ######################
-## 5. Connect to Network
+## 5. Arm
 ######################
-try:
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def arm():
+    if -yDeadZoneRight < yAxisRight < yDeadZoneLeft:
+        print "Arm: Stopped"
+    elif yAxisRight <= 0:
+        print "Arm: Forwards"
+    else:
+        print "Arm: Backwards"
 
-    # Connect the socket to the port on the server given by the caller
-    server_address = (sys.argv[1], 10000)
-    print >>sys.stderr, 'connecting to %s port %s' % server_address
-    sock.connect(server_address)
-except:
-    print "#" * 60
-    print "ERROR: Failed to connect to host"
-    print "#" * 60
-    exit()
 
+######################
+## 5. Grasper
+######################
+def grasper():
+    if triggerRight > -1.0:
+        print "Grasper: Closed"
+    else:
+        print "Grasper: Open"
+
+
+######################
+## 6. Connect to Network
+######################
+def connection(ip, port):
+    try:
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect the socket to the port on the server given by the caller
+        server_address = (ip, port)
+        print >>sys.stderr, 'connecting to %s port %s' % server_address
+        sock.connect(server_address)
+        return sock
+    except:
+        print "#" * 60
+        print "ERROR: Failed to connect to host"
+        print "#" * 60
+        sleep(2)
 
 ######################
 ##      Main        ##
 ######################
 ui()
+try:
+    sockDrive = connection(sys.argv[1], 10000)
+except:
+    print "No drive ip"
+try:
+    sockArm = connection(sys.argv[2], 10001)
+except:
+    print "No arm ip"
+sleep(1)
+
 while True:
     controllerInput()
-    drive = driveMotors()
-
-    try:
-        sock.sendall(str(int(KitBotSpeed(-drive[0]))) + ' ' + str(int(KitBotSpeed(drive[1]))))
-        sleep(socketRate)
-
-    except:
-        print "Error: Failed to connect to Robot"
+    if xbox:
+        print "Exiting...bye"
         exit()
 
-    os.system('clear')
-    print "#"*60
-    print "##", " "*20, "Motor Values", " " *20, "##"
-    print "#"*60
-    print "motorL: ", drive[0], "motorR: ", drive[1]
+    if start:
+        mode += 1
+        mode %= 2
+        sleep(1)
+
+    if mode:
+        drive = driveMotors()
+
+        try:
+            sockDrive.sendall(str(int(KitBotSpeed(-drive[0]))) + ' ' + str(int(KitBotSpeed(drive[1]))))
+            sleep(socketRate)
+
+        except:
+            print "Error: Failed to connect to Robot"
+            #exit()
+
+        os.system('clear')
+        print "#"*60
+        print "##", " "*20, "Motor Values", " " *20, "##"
+        print "#"*60
+        print "motorL: ", drive[0], "motorR: ", drive[1]
+    else:
+        os.system('clear')
+        arm()
+        grasper()
--- hostTest.py
+++ hostTest.py
@@ -1,18 +1,17 @@
 # --------------------------------File on Robot---------------------------------
 # Hosts a TCP connection and interprets the data recieved
 import sys, os, socket
-from pyax12.connection import Connection
+'''
+import RoboPiLib as RPL
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+'''
 
-sc = Connection(port="/dev/cu.usbmodem1421", baudrate=1000000)
-
-numMotor = 4
-
-def set_continuous(motor_id):
-    sc.set_cw_angle_limit(motor_id, 0, degrees=False)
-    sc.set_ccw_angle_limit(motor_id, 0, degrees=False)
-
-for i in range(1, numMotor):
-    set_continuous(i)
+######################
+## Global Variables ##
+######################
+# motor pins
+motorL = 0
+motorR = 1
 
 ######################
 ##    Host Info     ##
@@ -39,11 +38,12 @@ while True:
             data = connection.recv(9)
             data = data.split(' ')
 
-            sc.set_speed(1, int(data[0]))
-            sc.set_speed(4, int(data[0]))
+            '''
+            RPL.servoWrite(motorL, int(data[0]))
+            RPL.servoWrite(motorR, int(data[1]))
+            '''
 
-            sc.set_speed(2, int(data[1]))
-            sc.set_speed(3, int(data[1]))
+            print data
 
     finally:
         connection.close()
