d0972acd6ab40f5c29a686ba29bd04a020c52622
avoss19 <avoss19@bsmschool.org>
2018-06-07 19:59:20 -0500

    clean up

--- Controller.py
+++ Controller.py
@@ -68,7 +68,7 @@ def ui():
 ## 2. Controller Reading
 ######################
 def controllerInput():
-    global xAxisLeft, yAxisLeft, xAxisRight, yAxisRight, triggerLeft, triggerRight
+    global xAxisLeft, yAxisLeft, xAxisRight, yAxisRight
 
     dpadleft = 0
     dpadright = 0
